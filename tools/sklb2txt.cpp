f2509a9f58152d28e637838d4e0582834866cb16
Einar Johan T. Sømåen <einarjohants@gmail.com>
2011-08-11 01:15:29 +0200

    SKLB2TXT: Added Benjamin Haischs information to SKLB2TXT

--- tools/sklb2txt.cpp
+++ tools/sklb2txt.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <iostream>
 
+// Based on Benjamin Haischs work on sklb-files.
+
 using namespace std;
 
 int main(int argc, char **argv) {
@@ -26,6 +28,7 @@ int main(int argc, char **argv) {
 	char boneString[32];
 	char parentString[32];
 	
+	float x = 0, y = 0, z = 0, angle = 0;
 	// Bones are listed in the same order as in the meshb.
 	for(int i=0;i<numBones;i++){
 		file.read((char*)&boneString,32);
@@ -33,14 +36,19 @@ int main(int argc, char **argv) {
 		
 		std::cout << "# BoneName " << boneString << "\twith parent: " << parentString << "\t"; 
 		//file.seekg(28,ios::cur);
-		for(int i = 0; i < 7;i++) {
-			float x = 0;
-			file.read((char*)&x,4);
-			std::cout << x << " ";
-		}
-		
-		
-		std::cout << std::endl;
+		std::cout << " position: ";
+		file.read((char*)&x,4);
+		file.read((char*)&y,4);
+		file.read((char*)&z,4);
+		std::cout << x << " " << y << " " << z << " ";
+		std::cout << " rotation: ";
+		file.read((char*)&x,4);
+		file.read((char*)&y,4);
+		file.read((char*)&z,4);
+		std::cout << x << " " << y << " " << z << " angle: ";
+		file.read((char*)&angle,4);
+		std::cout << angle << std::endl;
+
 	}
 	/*
 	The last 28 bytes are, by looking at guy.sklb:
